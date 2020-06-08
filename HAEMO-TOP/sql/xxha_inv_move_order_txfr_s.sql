157ab1523a2d3b5596bad52907bf90a4ffaa9dd7
vsubramanian-haemonetics <62989438+vsubramanian-haemonetics@users.noreply.github.com>
2020-06-08 06:40:13 -0400

    xxha_inv_move_order_txfr_s-INC0319196

---
+++ HAEMO-TOP/sql/xxha_inv_move_order_txfr_s.sql
@@ -0,0 +1,8 @@
+CREATE SEQUENCE haemo.xxha_inv_move_order_txfr_s
+START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE ORDER
+
+
+/
+
+
+CREATE SYNONYM xxha_inv_move_order_txfr_s FOR  haemo.xxha_inv_move_order_txfr_s
